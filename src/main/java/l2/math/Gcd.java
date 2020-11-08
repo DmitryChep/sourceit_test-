b6698ce4617dcafd0b18c150f3d527d709e9c264
Dmitry <dmitrych5129@gmail.com>
2020-11-08 17:57:31 +0200

    Task1 refactoring

---
+++ src/main/java/l2/math/Gcd.java
@@ -0,0 +1,13 @@
+package l2.math;
+
+public class Gcd {
+    public int gcd(int a, int b) {
+        int r;
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
